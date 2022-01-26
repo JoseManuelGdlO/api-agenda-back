f75ca80ca9106964948db75c2d0897c0bf4708b9
Jose Manuel <josemanuelgarciadela@gmail.com>
2022-01-26 12:58:30 -0600

    ad

--- app/Models/client.php
+++ app/Models/client.php
@@ -5,13 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
-class Client extends Model
+class client extends Model
 {
     use HasApiTokens, HasFactory, Notifiable;
 
